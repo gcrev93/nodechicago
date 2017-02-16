a1a24d556db1ff8dff11ee0621666f0b4d017c21
Gabrielle Crevecoeur <gcrev93@gmail.com>
2017-02-16 15:54:39 -0500

    Update myweather.js

--- myweather.js
+++ myweather.js
@@ -17,8 +17,8 @@ var Particle = require("particle-io");
 
 var board = new five.Board({
   io: new Particle({
-    token: "4454731742f262fcb07e21106219cf024c7f5743",
-    deviceId: "350044000a47353138383138"
+    token: "",
+    deviceId: ""
   })
 });
 
