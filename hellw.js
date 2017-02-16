7f49fcd8e188f64c541fe7f08f89a6d7da4c5dc9
Gabrielle Crevecoeur <gcrev93@gmail.com>
2017-02-16 15:55:03 -0500

    Update hellw.js

--- hellw.js
+++ hellw.js
@@ -2,8 +2,8 @@ var five = require("johnny-five");
 var Particle = require("particle-io");
 var board = new five.Board({
   io: new Particle({
-    token: "4454731742f262fcb07e21106219cf024c7f5743",
-    deviceId: "350044000a47353138383138"
+    token: "",
+    deviceId: ""
   })
 });
 
