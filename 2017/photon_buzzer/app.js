a904dad1c584ba9dc64dea01704752696c17597c
Kevin Leung <leung.kevin23@gmail.com>
2017-06-30 14:58:29 -0500

    Working five and particle libraries with buzzer and button

---
+++ 2017/photon_buzzer/app.js
@@ -0,0 +1,39 @@
+// bring in required libraries and packages
+require('dotenv').config()
+var five = require('johnny-five')
+var Particle = require('particle-io')
+
+// set up and connect to the particle board
+var board = new five.Board({
+  io: new Particle({
+    // Token and Device ID can be found on particle.io IDE
+    // Particle Token: Settings -> Access Token
+    // Particle Device ID: Devices -> Device ID
+    token: process.env.PARTICLE_TOKEN,
+    deviceId: process.env.PARTICLE_DEVICE_ID
+  })
+})
+
+// run code on the particle
+// See for more information: https://github.com/rwaldron/particle-io
+board.on('ready', function () {
+  console.log('Setting pins..')
+  this.pinMode('D1', this.MODES.INPUT)
+  this.pinMode('D0', this.MODES.OUTPUT)
+
+  console.log('Device Ready..')
+  console.log('Reading Digital Pin D1..')
+
+  // Log all the readings for D1
+  this.digitalRead('D1', function (data) {
+    console.log('D1 value: ' + data)
+    if (data === 1) {
+      console.log('Buzzer.. ON')
+      this.digitalWrite('D0', 1)
+    }
+    if (data === 0) {
+      console.log('Buzzer.. OFF')
+      this.digitalWrite('D0', 0)
+    }
+  })
+})
